a1384e138c57bc4fe3cf083e23b83bc2527c0f80
Bernard Budano <bbudano@gmail.com>
2023-01-03 18:45:25 +0100

    Initial commit

---
+++ src/main/java/hr/bbudano/ela/ElaApplication.java
@@ -0,0 +1,13 @@
+package hr.bbudano.ela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/bbudano/ela/ElaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.bbudano.ela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
