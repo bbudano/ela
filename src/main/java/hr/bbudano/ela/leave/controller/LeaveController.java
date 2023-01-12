8b660e8a73ff9cc45185994c3e45237e8225ff3c
Bernard Budano <bbudano@gmail.com>
2023-01-12 13:34:52 +0100

    Added validation of CreateLeaveRequest

--- src/main/java/hr/bbudano/ela/leave/controller/LeaveController.java
+++ src/main/java/hr/bbudano/ela/leave/controller/LeaveController.java
@@ -3,6 +3,7 @@ package hr.bbudano.ela.leave.controller;
 import hr.bbudano.ela.leave.dto.CreateLeaveRequest;
 import hr.bbudano.ela.leave.dto.LeaveView;
 import hr.bbudano.ela.leave.service.LeaveService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ public class LeaveController {
     private final LeaveService leaveService;
 
     @PostMapping
-    public LeaveView createLeave(@RequestBody CreateLeaveRequest createLeaveRequest) {
+    public LeaveView createLeave(@Valid @RequestBody CreateLeaveRequest createLeaveRequest) {
         return leaveService.createLeave(createLeaveRequest);
     }
 
--- src/main/java/hr/bbudano/ela/leave/dto/CreateLeaveRequest.java
+++ src/main/java/hr/bbudano/ela/leave/dto/CreateLeaveRequest.java
@@ -1,6 +1,11 @@
 package hr.bbudano.ela.leave.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
-public record CreateLeaveRequest(Long leaveTypeId, LocalDate startDate, LocalDate endDate, String comment) {
+public record CreateLeaveRequest(@NotNull(message = "Leave type id can not be null") Long leaveTypeId,
+                                 @NotNull(message = "Start date can not be null") LocalDate startDate,
+                                 @NotNull(message = "End date can not be null") LocalDate endDate,
+                                 String comment) {
 }
