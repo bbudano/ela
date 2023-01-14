a2057ad20d381f1ea2e32570cae6dd029da232d7
Bernard Budano <bbudano@gmail.com>
2023-01-14 14:37:39 +0100

    Basic react router implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Admin from './components/Admin';
+import User from './components/User';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello</h1>
+      <Routes>
+        <Route path="admin" element={ <Admin /> } />
+        <Route path="user" element={ <User /> } />
+      </Routes>
     </div>
   );
 }
