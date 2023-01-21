1cdc427fe4f892cb4d116d510f4ba21bf535fe78
Bernard Budano <bbudano@gmail.com>
2023-01-21 15:46:22 +0100

    Added role field to UpdateEmployeeRequest

--- src/main/java/hr/bbudano/ela/employee/dto/UpdateEmployeeRequest.java
+++ src/main/java/hr/bbudano/ela/employee/dto/UpdateEmployeeRequest.java
@@ -1,5 +1,6 @@
 package hr.bbudano.ela.employee.dto;
 
+import hr.bbudano.ela.employee.model.Role;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -9,5 +10,6 @@ public record UpdateEmployeeRequest(@NotNull(message = "Email can not be null") 
                                     @NotNull(message = "Last name can not be null") String lastName,
                                     @NotNull(message = "Date of birth can not be null") LocalDate dateOfBirth,
                                     @NotNull(message = "Employment date can not be null") LocalDate employmentDate,
-                                    @NotNull(message = "Team id can not be null") Long teamId) {
+                                    @NotNull(message = "Team id can not be null") Long teamId,
+                                    Role role) {
 }
