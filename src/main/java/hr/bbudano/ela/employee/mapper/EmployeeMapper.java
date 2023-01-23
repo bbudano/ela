f0ee2ac1f25fc9824a72f0efab32a015ad0685bf
Bernard Budano <bbudano@gmail.com>
2023-01-23 22:26:22 +0100

    Added Employee to UserProfileDto mapping method

--- src/main/java/hr/bbudano/ela/employee/mapper/EmployeeMapper.java
+++ src/main/java/hr/bbudano/ela/employee/mapper/EmployeeMapper.java
@@ -4,7 +4,9 @@ import hr.bbudano.ela.employee.dto.CreateEmployeeRequest;
 import hr.bbudano.ela.employee.dto.EmployeeView;
 import hr.bbudano.ela.employee.dto.UpdateEmployeeRequest;
 import hr.bbudano.ela.employee.model.Employee;
+import hr.bbudano.ela.security.dto.UserProfileDto;
 import hr.bbudano.ela.team.mapper.TeamMapper;
+import hr.bbudano.ela.team.model.Team;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR,
@@ -14,12 +16,22 @@ public interface EmployeeMapper {
     @Mapping(target = "team", source = "team", qualifiedByName = "toTeamView")
     EmployeeView toEmployeeView(Employee employee);
 
+    @Mapping(target = "team", source = "team", qualifiedByName = "mapTeam")
+    UserProfileDto toUserProfileDto(Employee employee);
+
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "team", ignore = true)
+    @Mapping(target = "imageUrl", ignore = true)
     Employee toEmployee(CreateEmployeeRequest createEmployeeRequest);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "team", ignore = true)
+    @Mapping(target = "imageUrl", ignore = true)
     void mapEmployee(@MappingTarget Employee employee, UpdateEmployeeRequest updateEmployeeRequest);
+
+    @Named("mapTeam")
+    default String mapTeam(Team team) {
+        return team != null ? team.getName() : null;
+    }
 
 }
--- src/main/java/hr/bbudano/ela/security/service/UserService.java
+++ src/main/java/hr/bbudano/ela/security/service/UserService.java
@@ -1,5 +1,6 @@
 package hr.bbudano.ela.security.service;
 
+import hr.bbudano.ela.employee.mapper.EmployeeMapper;
 import hr.bbudano.ela.employee.repository.EmployeeRepository;
 import hr.bbudano.ela.exception.ElaException;
 import hr.bbudano.ela.security.dto.UserProfileDto;
@@ -12,18 +13,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
 
     public UserProfileDto getUserProfile(String principal) {
         var employee = employeeRepository.findByEmail(principal)
                 .orElseThrow(() -> new ElaException("Employee not found: " + principal, HttpStatus.INTERNAL_SERVER_ERROR));
 
-        return new UserProfileDto(
-                employee.getEmail(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getRole(),
-                employee.getTeam().getName(),
-                employee.getImageUrl());
+        return employeeMapper.toUserProfileDto(employee);
     }
 
 }
